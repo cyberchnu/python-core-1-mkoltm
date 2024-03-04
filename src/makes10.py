dad9c472ecfa4da3105daab51ea265c64929a737
Asus <koltativ.maksym@chnu.edu.ua>
2024-03-04 20:09:03 +0200

    Task 7 done

--- src/makes10.py
+++ src/makes10.py
@@ -1,3 +1,7 @@
 def makes10(a, b):
-  # Type your code
-  return 
+  return a == 10 or b == 10 or a + b == 10
+
+
+print(makes10(10, 5))  # Поверне True (10 дорівнює 10)
+print(makes10(2, 8))  # Поверне True (сума 2 та 8 дорівнює 10)
+print(makes10(2, 7))  # Поверне False (ні одне число не дорівнює 10, а їх сума не дорівнює 10)
